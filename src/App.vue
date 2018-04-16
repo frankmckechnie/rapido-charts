3185a68a35b531fbdbd60963ced3644b879e8001
MCKECHNIE Frank <fm17@pinsentmasons.com>
2018-04-16 16:38:53 +0100

    small update

--- src/App.vue
+++ src/App.vue
@@ -49,21 +49,24 @@ export default {
       title: { config: "Configuration" },
       labels: ["January", "February", "March", "April", "May", "June", "July"],
       dataSource: [
-          {
-            label: "Data One",
-            backgroundColor: [
-              "rgba(255, 99, 132, 1)",
-              "rgba(54, 162, 235, 1)",
-              "rgba(255, 206, 86, 1)",
-              "rgba(75, 192, 192, 1)",
-              "rgba(153, 102, 255, 1)",
-              "rgba(255, 159, 64, 1)",
-              "rgba(255, 15, 24, 1)"
-            ],
-            data: [40, 39, 10, 40, 39, 80, 40]
-          }
-        ],
-        maxDataSet: 5
+        {
+          label: "Data One",
+          backgroundColor: [
+            "rgba(255, 99, 132, 1)",
+            "rgba(54, 162, 235, 1)",
+            "rgba(255, 206, 86, 1)",
+            "rgba(75, 192, 192, 1)",
+            "rgba(153, 102, 255, 1)",
+            "rgba(255, 159, 64, 1)",
+            "rgba(255, 15, 24, 1)"
+          ],
+          data: [40, 39, 10, 40, 39, 80, 40]
+        }
+      ],
+      maxDataSet: 5,
+      heading: "",
+      labelx: "",
+      labely:""
     }
   },
   methods: {
