6e2dff89dd495a93949c0b31f55df90ae53f9432
frank <frankmckechnie@gmail.com>
2018-04-22 12:29:48 +0100

    add more functions to polyfill

--- src/mixins/polyfill.js
+++ src/mixins/polyfill.js
@@ -91,3 +91,20 @@ if (!Object.keys) {
     };
   }());
 }
+
+function msieversion() {
+
+    var ua = window.navigator.userAgent;
+    var msie = ua.indexOf("MSIE ");
+
+    if (msie > 0 || !!navigator.userAgent.match(/Trident.*rv\:11\./))  // If Internet Explorer, return version number
+    {
+        alert(parseInt(ua.substring(msie + 5, ua.indexOf(".", msie))));
+    }
+    else  // If another browser, return 0
+    {
+        alert('otherbrowser');
+    }
+
+    return false;
+}
